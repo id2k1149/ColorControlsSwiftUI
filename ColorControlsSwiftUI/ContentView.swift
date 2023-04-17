6c95e564b9343b036a688aaff1931b8bf1c02025
id2k1149 <id2k1149@gmail.com>
2023-04-17 17:21:29 -0400

    refactor

--- ColorControlsSwiftUI/ContentView.swift
+++ ColorControlsSwiftUI/ContentView.swift
@@ -11,64 +11,63 @@ struct ContentView: View {
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
+    
     @State private var redDigitalValue = ""
     @State private var greenDigitalValue = ""
     @State private var blueDigitalValue = ""
-    @State private var alertPresented = false
+    
+//    @State private var alertPresented = false
     
     var body: some View {
         ZStack {
             Color(.cyan).ignoresSafeArea()
             
             VStack {
-                HStack {
-                    Text("\(lround(redSliderValue))").font(.largeTitle)
-                    Text("\(lround(greenSliderValue))").font(.largeTitle)
-                    Text("\(lround(blueSliderValue))").font(.largeTitle)
-                }
+//                HStack {
+//                    Text("\(lround(redSliderValue))").font(.largeTitle)
+//                    Text("\(lround(greenSliderValue))").font(.largeTitle)
+//                    Text("\(lround(blueSliderValue))").font(.largeTitle)
+//                }
                 
                 Rectangle()
                     .frame(height: 200)
-                    .cornerRadius(25)
                     .foregroundColor(Color(red: redSliderValue / 255,
                                            green: greenSliderValue / 255,
                                            blue: blueSliderValue / 255,
                                            opacity: 1))
-                
+                   
+                    .cornerRadius(25)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.white, lineWidth: 4)
+                    )
+
                 SliderView(value: $redSliderValue,
                            digitalValue: $redDigitalValue,
-                           textColor: .red)
+                           sliderColor: .red)
                 
                 SliderView(value: $greenSliderValue,
                            digitalValue: $greenDigitalValue,
-                           textColor: .green)
+                           sliderColor: .green)
                 
                 SliderView(value: $blueSliderValue,
                            digitalValue: $blueDigitalValue,
-                           textColor: .blue)
+                           sliderColor: .blue)
                 
+                /*
                 Button("Done", action: checkDigitalValue)
                     .alert(
                         "Wrong Format",
                         isPresented: $alertPresented,
                         actions: {}
                     ) { Text("Enter value from 0 to 255") }
+                 */
 
                 Spacer()
             }
             .padding()
         }
         
-    }
-    
-    private func checkDigitalValue() {
-        if let _ = Double(redDigitalValue) {
-            redSliderValue = Double(redDigitalValue) ?? 0
-        } else {
-            alertPresented.toggle()
-        }
-        redDigitalValue = ""
-        return
     }
 }
 
--- ColorControlsSwiftUI/SliderView.swift
+++ ColorControlsSwiftUI/SliderView.swift
@@ -10,18 +10,69 @@ import SwiftUI
 struct SliderView: View {
     @Binding var value: Double
     @Binding var digitalValue: String
-    let textColor: Color
+    let sliderColor: Color
     
     var body: some View {
         HStack {
-            Text("0").foregroundColor(textColor)
+//            Text("0")
+//                .foregroundColor(sliderColor)
+//                .bold()
+            HStack {
+                Text("\(sliderColor.description):")
+                    .bold()
+
+                Spacer()
+                Text("\(lround(value))")
+                    .bold()
+            }
+            .frame(width: 96)
+            
             Slider(value: $value, in: 0...255, step: 1)
-                .accentColor(textColor)
-            Text("255").foregroundColor(textColor)
-            TextField("\(lround(value))", text: $digitalValue)
+                .accentColor(sliderColor)
+//            Text("255")
+//                .foregroundColor(sliderColor)
+//                .bold()
+            TextField("", text: $digitalValue)
                 .frame(width: 48)
-                .textFieldStyle(.roundedBorder)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .onAppear {
+//                    digitalValue = "\(lround(value))"
+//                }
+                .keyboardType(.numberPad)
+                .overlay(
+                    Text("\(lround(value))")
+                        .foregroundColor(.black)
+                        .font(.system(size: 16))
+                        .bold()
+                        .opacity(digitalValue.isEmpty ? 1 : 0)
+                )
+                .onSubmit {
+                    // Add your action here
+                    if let _ = Double(digitalValue) {
+                        value = Double(digitalValue) ?? 0
+                    }
+                    digitalValue = ""
+                }
+            /*
+                .onChange(of: digitalValue) { newValue in
+                    if let intValue = Int(newValue), (0...255).contains(intValue) {
+                        print("Valid value entered: \(intValue)")
+                        // Add your valid value action here
+                    } else {
+                        print("Invalid value entered: \(newValue)")
+                        // Add your invalid value action here, such as showing an alert
+                    }
+                }
+             */
+
             
         }
+
+    }
+}
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
