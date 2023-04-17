6f7501d77e4abc83f14c083901b5df1b65c67d58
id2k1149 <id2k1149@gmail.com>
2023-04-17 08:40:33 -0400

    Initial Commit

---
+++ ColorControlsSwiftUI/ColorControlsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorControlsSwiftUIApp.swift
+//  ColorControlsSwiftUI
+//
+//  Created by Max Franz Immelmann on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorControlsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
